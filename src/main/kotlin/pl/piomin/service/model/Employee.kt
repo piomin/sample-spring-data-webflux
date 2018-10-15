ed7fc673f0976d0e0d2d512a09dfdc6a2074599b
piotr.minkowski <piotr.minkowski@gmail.com>
2018-10-15 17:19:59 +0200

    removing sql

--- src/main/kotlin/pl/piomin/service/model/Employee.kt
+++ src/main/kotlin/pl/piomin/service/model/Employee.kt
@@ -1,4 +1,6 @@
 package pl.piomin.service.model
 
-data class Employee(val id: Int, val name: String, val salary: Int) {
+import org.springframework.data.annotation.Id
+
+data class Employee(@Id val id: Int, val name: String, val salary: Int) {
 }
